0392f99cc82832fc6f30e259e75500e7d501fc77
RemiSaurel <r.saurel5@gmail.com>
2024-03-17 00:15:39 +0100

    chore: setup gateway

--- src/main/java/fr/miage/acm/apigateway/ApiGatewayApplication.java
+++ src/main/java/fr/miage/acm/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,30 @@ package fr.miage.acm.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.http.HttpMethod;
+
 
 @SpringBootApplication
+@EnableWebFluxSecurity
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		http
+				.authorizeExchange(exchanges -> exchanges
+						.pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+						.pathMatchers(HttpMethod.GET, "/**").permitAll()
+						.anyExchange().permitAll()
+				);
+		return http.build();
 	}
 
 }
