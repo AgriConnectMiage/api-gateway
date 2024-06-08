38ca9348c1234197ca45492b2db09ca1c301b9db
RemiSaurel <r.saurel5@gmail.com>
2024-06-08 23:51:36 +0200

    chore: cors config (wip)

--- src/main/java/fr/miage/acm/apigateway/ApiGatewayApplication.java
+++ src/main/java/fr/miage/acm/apigateway/ApiGatewayApplication.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.http.HttpMethod;
 
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableWebFluxSecurity
@@ -23,9 +27,24 @@ public class ApiGatewayApplication {
 				.authorizeExchange(exchanges -> exchanges
 						.pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 						.pathMatchers(HttpMethod.GET, "/**").permitAll()
+						.pathMatchers(HttpMethod.POST, "/**").permitAll()
+						.pathMatchers(HttpMethod.PUT, "/**").permitAll()
+						.pathMatchers(HttpMethod.DELETE, "/**").permitAll()
 						.anyExchange().permitAll()
 				);
 		return http.build();
 	}
 
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*")); // Allow all origins, customize as needed
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+
+		return new CorsWebFilter(source);
+	}
 }
