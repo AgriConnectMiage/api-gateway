d1f660817262dc71f444e3793639babbc568c08e
RemiSaurel <r.saurel5@gmail.com>
2024-06-09 10:51:04 +0200

    chore: cors / csrf config working

--- src/main/java/fr/miage/acm/apigateway/ApiGatewayApplication.java
+++ src/main/java/fr/miage/acm/apigateway/ApiGatewayApplication.java
@@ -2,20 +2,21 @@ package fr.miage.acm.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.http.HttpMethod;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableWebFluxSecurity
 public class ApiGatewayApplication {
+
+	@Bean
+	public GlobalFilter customFilter() {
+		return new PreFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
@@ -31,20 +32,8 @@ public class ApiGatewayApplication {
 						.pathMatchers(HttpMethod.PUT, "/**").permitAll()
 						.pathMatchers(HttpMethod.DELETE, "/**").permitAll()
 						.anyExchange().permitAll()
-				);
+				)
+				.csrf().disable();
 		return http.build();
-	}
-
-	@Bean
-	public CorsWebFilter corsWebFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowedOrigins(Arrays.asList("*")); // Allow all origins, customize as needed
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfiguration);
-
-		return new CorsWebFilter(source);
 	}
 }
